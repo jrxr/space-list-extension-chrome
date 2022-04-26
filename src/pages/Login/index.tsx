a70cf4c6b5d4aeaafe7e821e0bea506bb00c13e0
Agacy junior <juniormartinssom@gmail.com>
2022-04-26 19:41:39 -0300

    Criação do input

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,7 @@
 import { Container, FormContainer, PageTitle } from './styles';
 import Logo from '../../assets/logo.svg?component';
+import { HiOutlineLockClosed, HiOutlineMail } from 'react-icons/hi';
+import { Input } from '../../components/Input';
 
 export function Login() {
   return (
@@ -7,7 +9,10 @@ export function Login() {
       <Logo />
       <PageTitle>Login</PageTitle>
       <FormContainer>
-        
+        <Input  
+          icon={<HiOutlineMail />}
+          label="E-mail"
+          placeholder="your@email.com" />
       </FormContainer>
     </Container>
   )
